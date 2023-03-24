4e1c915987efe5cf53dbd0ae585c26ea96118716
Ahmad Fadillah <13020180149@umi.ac.id>
2023-03-24 23:33:31 +0800

    Update

--- backend/app/Http/Controllers/Report/ReportController.php
+++ backend/app/Http/Controllers/Report/ReportController.php
@@ -3515,4 +3515,103 @@ class ReportController extends ApiController{
         // dd(isset($res['d'][0]));
         return view('report.cetak-labelkecil-apotik', compact('res'));
     }
+
+    public function ringkasanPasienMasukKeluar(Request $request) {
+        $nocm = $request['nocm'];
+        $norec = $request['emr'];
+        $kdProfile = (int) $request['kdprofile'];
+
+        $data = DB::select(DB::raw(
+            "
+            SELECT
+                epd.emrdfk,
+                ep.noemr,
+                ed.TYPE,
+                pa.namapasien,
+                TO_CHAR(pa.tgllahir, 'DD-MM-YYYY') as tgllahir,
+                pa.nohp,
+                pa.nocm,
+                ep.jeniskelamin,
+                ep.umur,
+                pa.noidentitas,
+                al.alamatlengkap,
+                ep.noregistrasifk as noregistrasi , TO_CHAR(pr.tglregistrasi, 'DD-MM-YYYY HH24:MM:SS') as tglregistrasi,
+                epd.value,ep.namaruangan,pg.namalengkap as namadokter, epd.tgl,
+                --ap.noasuransi,ap.namapeserta,
+                pdd.pendidikan,pk.pekerjaan,ag.agama,sp.statusperkawinan
+                --case when ed.TYPE = 'datetime' then TO_CHAR(TO_TIMESTAMP(epd.value, 'YYYY-MM-DD HH24:MI:SS'),'YYYY-MM-DD HH24:MI:SS') else epd.value end as value
+            FROM
+                emrpasien_t AS ep
+                INNER JOIN emrpasiend_t AS epd ON ep.noemr = epd.emrpasienfk
+                INNER JOIN emrd_t AS ed ON epd.emrdfk = ed.ID
+                    INNER JOIN antrianpasiendiperiksa_t AS pd ON pd.norec = ep.norec_apd
+                    INNER JOIN pasiendaftar_t AS pr ON pr.norec = pd.noregistrasifk
+                left JOIN pegawai_m AS pg ON pg.id = pd.objectpegawaifk
+                left JOIN pasien_m as pa on ep.nocm =  pa.nocm
+                left JOIN alamat_m as al on pa.id = al.nocmfk
+                left JOIN pendidikan_m as pdd on pa.objectpendidikanfk = pdd.id
+                left JOIN pekerjaan_m as pk on pa.objectpekerjaanfk = pk.id
+                left JOIN agama_m as ag on pa.objectagamafk = ag.id
+                left JOIN statusperkawinan_m as sp on pa.objectstatusperkawinanfk = sp.id
+                -- left JOIN asuransipasien_m AS ap ON ap.nocmfk = pr.nocmfk
+            WHERE
+                ep.norec = '$norec'
+                    AND ep.kdprofile = '$kdProfile' 
+                AND epd.statusenabled = TRUE 
+                and epd.emrfk = $request[emrfk]
+                and pa.statusenabled = TRUE
+                
+                ORDER BY
+                ed.nourut
+                "
+        ));
+        // dd($data);
+        foreach ($data as $z) {
+            if ($z->type == "datetime") {
+                $z->value = date('Y-m-d H:i:s', strtotime($z->value));
+            }
+        }
+        $pageWidth = 500;
+        $res['profile'] = Profile::where('id', $request['kdprofile'])->first();
+
+        $res['d'] = $data;
+        $noemrpasien = '';
+        if (count($data) == 0) {
+            $noemrpasien = $request['emr'];
+        } else {
+            $noemrpasien = $data[0]->noemr;
+        }
+
+        return view('report.cetak-ringkasan-pasien-masuk-keluar', compact('res', 'pageWidth'));
+    }
+
+    public function konsulDokter(Request $request)
+    {
+        $res = array($request->all());
+        $norec = $request['emr'];
+        $kdProfile = (int) $request['kdprofile'];
+        $data = DB::table('pasien_m')->where('nocm', $request->nocm)->where('statusenabled', 't')->select(
+            'nocm as norm',
+            'namapasien as namalengkap',
+            'tgllahir',
+            'noidentitas'
+            )->first();
+        
+        $datadaridokter = DB::table('pegawai_m')
+        ->join('unitkerjapegawai_m', 'pegawai_m.objectunitkerjafk', '=', 'unitkerjapegawai_m.id')->select('unitkerjapegawai_m.namaexternal')
+        ->where('namalengkap', $request->daridokter)->get();
+
+        $datauntukdokter = DB::table('pegawai_m')
+        ->join('unitkerjapegawai_m', 'pegawai_m.objectunitkerjafk', '=', 'unitkerjapegawai_m.id')->select('unitkerjapegawai_m.namaexternal')
+        ->where('namalengkap', $request->untukdokter)->get();
+    
+
+        $res['profile'] = Profile::where('id', $request['kdprofile'])->first();
+
+        $res['d'] = $data;
+
+        // dd($res);
+
+        return view('report.cetak-konsul-dokter', compact('res', 'datadaridokter', 'datauntukdokter'));
+    }
 }
