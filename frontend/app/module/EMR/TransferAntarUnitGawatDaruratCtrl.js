1560b8ebca81e93c72f53ed7914b0ee97a90108a
Ahmad Fadillah <13020180149@umi.ac.id>
2023-04-12 14:23:32 +0800

    index

--- frontend/app/module/EMR/TransferAntarUnitGawatDaruratCtrl.js
+++ frontend/app/module/EMR/TransferAntarUnitGawatDaruratCtrl.js
@@ -3,7 +3,7 @@ define(['initialize'], function (initialize) {
     initialize.controller('TransferAntarUnitGawatDaruratCtrl', ['$q', '$rootScope', '$scope', 'ModelItem', '$state', 'CacheHelper', 'DateHelper', 'MedifirstService',
         function ($q, $rootScope, $scope, ModelItem, $state, cacheHelper, dateHelper, medifirstService) {
 
-
+            var paramsIndex = $state.params.index ? parseInt($state.params.index) : null
             $scope.noCM = $state.params.noCM;
             $scope.tombolSimpanVis = true
             $scope.noRecPap = cacheHelper.get('noRecPap');
@@ -236,7 +236,7 @@ define(['initialize'], function (initialize) {
                 })
 
                 for (var i = 0; i <= dataLoad.length - 1; i++) {
-                    if (parseFloat($scope.cc.emrfk) == dataLoad[i].emrfk) {
+                    if (parseFloat($scope.cc.emrfk) == dataLoad[i].emrfk && paramsIndex == dataLoad[i].index) {
 
                         if (dataLoad[i].type == "textbox") {
                             $scope.item.obj[dataLoad[i].emrdfk] = dataLoad[i].value
@@ -308,6 +308,7 @@ define(['initialize'], function (initialize) {
                     arrSave.push({ id: arrobj[i], values: $scope.item.obj[parseInt(arrobj[i])] })
                 }
                 $scope.cc.jenisemr = 'asesmen'
+                $scope.cc.index = $state.params.index;
                 var jsonSave = {
                     head: $scope.cc,
                     data: arrSave
