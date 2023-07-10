2dc8565160673cfc4cb01bbb9d2498bb9084257c
resna <resnapangestu01@gmail.com>
2023-07-10 16:25:55 +0700

    -

--- backend/app/Http/Controllers/TataRekening/TagihanController.php
+++ backend/app/Http/Controllers/TataRekening/TagihanController.php
@@ -3386,11 +3386,11 @@ class TagihanController  extends ApiController
                         $end    = new Carbon($tglAwalDiskon);
                         $tglRegis = date('Y-m-d', strtotime($dateAPD->tglregistrasi));
                         $selisihjam = $start->diff($end)->format('%H');
-                        if ($tglRegis == $itemDate){
-                            if ((int)$selisihjam <= 6 ){
-                                $diskon = ((float)$sirahMacan[0]->hargasatuan * 50)/100;
-                            }
-                        }
+                        // if ($tglRegis == $itemDate){
+                        //     if ((int)$selisihjam <= 6 ){
+                        //         $diskon = ((float)$sirahMacan[0]->hargasatuan * 50)/100;
+                        //     }
+                        // }
                         // ## END ##
 
 
