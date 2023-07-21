92d1cc19f59c3928772359354a1dfce911117635
Ahmad Fadillah <13020180149@umi.ac.id>
2023-07-21 15:26:02 +0800

    -

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -2792,6 +2792,7 @@ Route::group(['middleware' => 'cors', 'prefix' => 'service'], function () {
         Route::get('report/cetak-rinc-billing', 'Report\ReportController@cetakRincBilling');
         Route::get('report/cetak-surat-keterangan-kontrol', 'Report\ReportController@suratKeteranganKontrol');
         Route::get('report/cetak-asesmen-awal-medis-rajal', 'Report\ReportController@asesmenAwalMedisRajal');
+        Route::get('report/cetak-emr-all-page', 'Report\ReportController@emrAllPage');
 
         Route::get('viewer/get-list-antrian','Antrian\AntrianController@getListAntrian');
         Route::get('viewer/update-antrian','Antrian\AntrianController@updatePanggil');
