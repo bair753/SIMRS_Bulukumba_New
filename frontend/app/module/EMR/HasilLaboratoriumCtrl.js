5439bed4549244d7e007849900d9843f39aefd61
ahmadfadillllah <ahmadfadillah502@gmail.com>
2023-09-25 10:53:30 +0800

    -

--- frontend/app/module/EMR/HasilLaboratoriumCtrl.js
+++ frontend/app/module/EMR/HasilLaboratoriumCtrl.js
@@ -1,6 +1,6 @@
 define(['initialize'], function (initialize) {
     'use strict';
-    initialize.controller('HasilEndoskopiCtrl', ['$q', '$rootScope', '$scope', 'ModelItem', '$state', 'CacheHelper', 'DateHelper', 'MedifirstService',
+    initialize.controller('HasilLaboratoriumCtrl', ['$q', '$rootScope', '$scope', 'ModelItem', '$state', 'CacheHelper', 'DateHelper', 'MedifirstService',
         function ($q, $rootScope, $scope, ModelItem, $state, cacheHelper, dateHelper, medifirstService) {
 
 
