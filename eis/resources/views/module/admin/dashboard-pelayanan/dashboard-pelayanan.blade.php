e89533af38368459814a51ae649bf854e8eea7ed
ahmadfadillllah <ahmadfadillah502@gmail.com>
2023-10-06 14:54:57 +0800

    Menampilkan trend pegunjung RJ, Detail Cara Daftar, Daftar Registrasi Pasien di EIS

--- eis/resources/views/module/admin/dashboard-pelayanan/dashboard-pelayanan.blade.php
+++ eis/resources/views/module/admin/dashboard-pelayanan/dashboard-pelayanan.blade.php
@@ -772,9 +772,9 @@
                         fillColor: {
                             linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
                             stops: [
-                                [0, Highcharts.getOptions().colors[0]],
+                                // [0, Highcharts.getOptions().colors[0]],
                                 // [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                                [1, Highcharts.Color(Highcharts.getOptions().colors[0])]
+                                // [1, Highcharts.Color(Highcharts.getOptions().colors[0])]
                             ]
                         },
                         lineWidth: 1,
